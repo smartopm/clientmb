542e26ab2beffe950072c04ae75d68697db65e16
Olivier JM Maniraho <manolivier93@gmail.com>
2022-09-06 16:56:28 +0200

    add basic navigation

--- App.tsx
+++ App.tsx
@@ -4,25 +4,60 @@ import {
   ApplicationProvider,
   IconRegistry,
   Layout,
-  Button, Icon
+  Text,
+  Icon,
+  Button,
 } from '@ui-kitten/components';
 import { IonIconsPack } from './icons';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DetailsScreen from './DetailsScreen';
 
-export const HomeIcon = (props) => (
-  <Icon {...props} name='home' />
-);
+const Stack = createNativeStackNavigator();
 
-const HomeScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Button accessoryLeft={HomeIcon}>Home</Button>
+export const HomeIcon = (props) => <Icon {...props} name="home" />;
+
+const HomeScreen = ({ navigation }) => (
+  <Layout style={styles.container}>
+    <Layout style={styles.layout} level="2">
+      <Text>2</Text>
+    </Layout>
+    <Layout style={styles.layout} level="1">
+      <Button onPress={() => navigation.navigate('Details')}>Go to Details </Button>
+    </Layout>
   </Layout>
 );
 
 export default () => (
-  <>
+  <NavigationContainer>
     <IconRegistry icons={[IonIconsPack]} />
     <ApplicationProvider {...eva} theme={eva.light}>
-      <HomeScreen />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+
+      {/* <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <HomeScreen />
+        <HomeScreen />
+      </Layout> */}
     </ApplicationProvider>
-  </>
+  </NavigationContainer>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  layout: {
+    flex: 2,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: 200,
+    height: 200,
+    margin: 20,
+  },
+});
---
+++ DetailsScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from '@ui-kitten/components';
+import { View } from 'react-native';
+
+export default function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
